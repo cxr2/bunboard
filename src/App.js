cf9005dc110c62d6c4140b8c47ba680f6924ffae
cxr2 <ciosahoulihan@gmail.com>
2023-02-03 12:27:09 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { AuthenticatedApp } from "./components/AuthenticatedApp";
 import { UnauthenticatedApp } from "./components/UnauthenticatedApp";
 import { useAuth } from "./hooks/useAuth";
+import bunlogo from "./images/bun.png";
 import "./App.css";
 
 function App() {
@@ -8,7 +9,11 @@ function App() {
 
   return (
     <div className="container">
-      <h1>💬 Chat Room</h1>
+      <h1>
+        <img src={bunlogo} className="bunlogo" alt="pixelated bun" />
+        Bun Board
+        <img src={bunlogo} className="bunlogo" alt="pixelated bun" />
+      </h1>
       {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
